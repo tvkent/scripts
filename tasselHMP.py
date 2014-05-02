cc34c1c265544e64ad8f270beeab1548d132431b
tvkent <tvkent@ucdavis.edu>
2014-05-01 19:19:26 -0700

    Update tasselHMP.py

--- tasselHMP.py
+++ tasselHMP.py
@@ -8,21 +8,21 @@ def Dict(jiao, ames):
     for line in jiao:
         newline=line.split() #split line by tabs to make list
         try:
-            jiaokeys.append(int(newline[0])) #add snp pos to growing list
+            jiaokeys.append(int(newline[3])) #add snp pos to growing list
         except ValueError:
             #do nothing--->don't need the header
             b=0
-        Jiao[newline[0]]=newline #create dictionary w/ snp pos as key and whole line as list as value
-        
+        Jiao[newline[3]]=newline #create dictionary w/ snp pos as key and whole line as list as value
+
     Ames={}
     ameskeys=[]
     for line in ames:
         newline=line.split()
         try:
-            ameskeys.append(int(newline[0]))
+            ameskeys.append(int(newline[3]))
         except ValueError:
             b=0
-        Ames[newline[0]]=newline
+        Ames[newline[3]]=newline
     jiaokeys.sort()
     ameskeys.sort()
     return(Jiao, Ames, jiaokeys, ameskeys)
@@ -39,7 +39,7 @@ def Key_Gen(Jiao, Ames, jiaokeys, ameskeys):
     return(finalkeys)
 
 def Insert_Line(jiao,ames,Jiao,Ames,jiaokeys,ameskeys,finalkeys):
-    outfile='/home/tyler/testout.txt' #final file for tassel use
+    outfile='AMES10.hmp.txt' #final file for tassel use
     outfile=open(outfile,'w')
     newline=ames[0].split() #split header 
     newline='\t'.join(newline) #and tab delimit for consistancy 
@@ -51,14 +51,14 @@ def Insert_Line(jiao,ames,Jiao,Ames,jiaokeys,ameskeys,finalkeys):
             outfile.write(line+'\n')
         if key not in ameskeys: #add missing data for positions not in ames data set
             length=ames[0].split()
-             #add correct number of N
+#add correct number of N
             outfile.write('S10_'+str(key)+'\t'+'NA\t'+'10\t'+str(key)+'\t'+'+\t'+'NA\t'+'NA\t'+'NA\t'+'NA\t'+'NA\t'+'NA\t')
-            for i in range(len(length)-1):
+            for i in range(len(length)-11):
                 outfile.write('N\t')
             outfile.write('\n')
-        
-jiao='/home/tyler/test1.txt' #file for donor creation
-ames='/home/tyler/test2.txt' #file to be imputed
+
+jiao='Jiao_ch10.hmp.txt' #file for donor creation
+ames='AmesUSInbreds_AllZeaGBSv1.0_imputed-130508/AmesUSInbreds_AllZeaGBSv1.0_imputed_20130508_chr10.hmp.txt' #file to be imputed
 jiao=open(jiao,'r')
 ames=open(ames,'r')
 jiao=jiao.readlines() #create list of lines
